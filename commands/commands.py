252e4c5365ae904167b4336cadc65ebfd8a4715a
Maxwell <msf9542@g.rit.edu>
2020-11-06 21:51:05 -0500

    minor changes

--- commands/commands.py
+++ commands/commands.py
@@ -1,6 +1,7 @@
 from commands.commands_src import *
 commands = {"time": time,
             "echo": echo,
+            "clear": clear,
             "show": None,
             "console": None,
             "upload": None,
