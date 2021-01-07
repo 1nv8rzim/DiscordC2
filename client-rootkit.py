659fbf32297f56f7da5b7a9719804416f4596be3
Maxwell <msf9542@g.rit.edu>
2021-01-06 19:49:01 -0500

    testing

--- client-rootkit.py
+++ client-rootkit.py
@@ -3,6 +3,12 @@ from socket import socket
 from string import ascii_letters, digits
 from os import getpid
 from random import choice
+from time import sleep
 
 token = ''.join(choice(ascii_letters + digits) for _ in range(72))
 pid = getpid()
+
+print(pid)
+
+while True:
+    sleep(1000)
